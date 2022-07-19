e7fd634d6f952719ca9da856add292bea9b01e59
FelixvL <digitaleservice@gmail.com>
2022-07-19 09:01:30 +0200

    next

--- app.py
+++ app.py
@@ -5,10 +5,26 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     detekst = """
+<style>
+    h3{
+        color:orange;
+    }    
+</style>
 <div>
-    <h1>Tafel</h1>
-    <img src="tafel.jpg">
+    <h1>Dit zijn onze meubels</h1>
+</div>
+<div>
+    <h3>Tafel</h3>
+    <img src="tafel.jpg" width="150px">
 
+</div>
+<div>
+    <h3>Stoel</h3>
+    <img src="stoel.jpg" width="150px">
+
+</div>
+<div>
+    <hr>
 </div>
 """
     return detekst
