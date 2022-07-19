292b718604444eb71d28837f45eaeaa46cda60a5
FelixvL <digitaleservice@gmail.com>
2022-07-19 09:45:33 +0200

    next

--- app.py
+++ app.py
@@ -18,33 +18,44 @@ mycursor = mydb.cursor()
 def hello_world():
     mycursor.execute("SELECT * FROM meubel")
     myresult = mycursor.fetchall()
-    for x in myresult:
-        print(x[1])
-
-    detekst = """
+    boventekst = """
 <style>
-    h3{
-        color:orange;
-    }    
+h3{
+    color:orange;
+}    
 </style>
 <div>
     <h1>Dit zijn onze meubels</h1>
 </div>
+    """
+    tussentekst = ""
+    for x in myresult:
+        #print(x[1])
+        tussentekst += """
 <div>
-    <h3>Tafel</h3>
-    <img src="tafel.jpg" width="150px">
-
+    <h3>"""
+        tussentekst += x[2]
+        tussentekst += """</h3>
+    <img src="""
+        tussentekst += x[1]
+        tussentekst += """ width="150px">
+</div>"""
+    ondertekst = """
+<div>
+    <hr>
 </div>
+    """
+
+    detekst = """
+
+
 <div>
     <h3>Stoel</h3>
     <img src="stoel.jpg" width="150px">
 
 </div>
-<div>
-    <hr>
-</div>
 """
-    return detekst
+    return boventekst + tussentekst + ondertekst
 
 
 #    SELECT * FROM meubel
